fc40c9b9578a887ca974c4a71efde7446ed917c9
Dylan Sotomayor <dylants365@gmail.com>
2019-07-07 03:11:37 -0700

    working gob encoding can send info from shell to test server

--- main.go
+++ main.go
@@ -13,32 +13,48 @@ import (
 	"net"
 )
 
-// maybe set conn and err up here
+var conn net.Conn
+var err error
 
-type job struct {
+type sJob struct {
 	ID int
 	Command string
 	Status string
 }
 
-var commands map[int]func(pid int64) //create a map for storing commands
+var commands map[int]func(pid int64, job sJob, enc *gob.Encoder) (sJob) //create a map for storing commands
 
 const sockAddr = "/tmp/task_master.sock" //Socket address for taskmaster 
 // type commandTable func(pid int64)
 
 func init() {
-	commands = make(map[int]func(pid int64))
-	commands[1] = func (int64){					//stop
-		fmt.Println("stop called")
+	commands = make(map[int]func(pid int64, job sJob, enc *gob.Encoder)(sJob))
+	commands[1] = func (pid int64, job sJob, enc *gob.Encoder) (sJob) {				//stop
+		fmt.Println("stop called in client")
 		time.Sleep(1000 * time.Millisecond)
+		job.ID = 1
+		job.Command = "stop called in server"
+		job.Status = "stopped"
+		enc.Encode(job)
+		return job
 	}
-	commands[2] = func (int64){					//start
-		fmt.Println("start called")
+	commands[2] = func (pid int64, job sJob, enc *gob.Encoder) (sJob) {				//start
+		fmt.Println("start called in client")
 		time.Sleep(1000 * time.Millisecond)
+		job.ID = 2
+		job.Command = "start called in server"
+		job.Status = "started"
+		enc.Encode(job)
+		return job
 	}
-	commands[3] = func (int64){					//kill
-		fmt.Println("kill called")
+	commands[3] = func (pid int64, job sJob, enc *gob.Encoder) (sJob) {				//kill
+		fmt.Println("kill called in client")
 		time.Sleep(1000 * time.Millisecond)
+		job.ID = 3
+		job.Command = "kill called in server"
+		job.Status = "killed"
+		enc.Encode(job)
+		return job
 	}
 }
 
@@ -53,12 +69,25 @@ func validInputCheck(n int) (bool) {
 }
 
 func main() {
-	var jobs []*job // thinking about how to make an array/slice of jobs
+	var job sJob
+	// var jobs []sJob // thinking about how to make an array/slice of jobs
 	// jobs = append(jobs, newjob)
 	// jobs[2]
-	conn, err := net.Dial("unix", sockAddr)
+	//start taskmaster server/daemon if no started
+	conn, err = net.Dial("unix", sockAddr)
+	if err != nil {
+		cmd := exec.Command("go run server/test_server.go &")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+		conn, err = net.Dial("unix", sockAddr)
+		if err != nil {
+			fmt.Printf("Error: unable to connect to taskmaster daemon at %s\n", sockAddr)
+			time.Sleep(1000 * time.Millisecond)
+			return
+		}
+		fmt.Printf("Connected to taskmaster daemon at %s\n", sockAddr)
+	}
 	enc := gob.NewEncoder(conn)
-	enc.Encode(jobs[2])    // these past three lines were mainly just written to get an idea
 	for ;true;{
 		var cmdNum int
 		clearwindow()
@@ -69,7 +98,7 @@ func main() {
 		fmt.Printf("Commands: %v, %v, %v\n\n", "1:start", "2:stop", "3:kill")
 		fmt.Print("Please choose a command number: ")
 		read := bufio.NewReader(os.Stdin)
-
+		
 		line, err := read.ReadString('\n')
 		
 		cmdNum, _ = strconv.Atoi(strings.TrimSuffix(line, "\n"))
@@ -83,12 +112,13 @@ func main() {
 			break
 		} else {
 			clearwindow()
-			fmt.Printf("%-12v%-12v%-12v\n", "ID", "Command", "Status")
-			fmt.Print("Please enter a pid: ")
+			// jobs[0] = append()
+			// fmt.Printf("%-12v%-12v%-12v\n", "ID", "Command", "Status")
+			// fmt.Print("Please enter a pid: ")
 			// line, err := read.ReadString('\n')
 			// check pid against vallid pids
 			command := commands
-			command[cmdNum](1/* actual pid goes here*/)
+			_ = command[cmdNum](1, job /* actual pid goes here*/, enc)
 		}
 	}
 }
